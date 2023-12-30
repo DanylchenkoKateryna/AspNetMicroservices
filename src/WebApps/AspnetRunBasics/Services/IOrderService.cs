8b44ac00973bebc385461193db2b0c503496a5ce
Kateryna Danylchenko <kdanilchenko265@gmail.com>
2023-12-30 20:49:09 +0200

    implement WebApp

---
+++ src/WebApps/AspnetRunBasics/Services/IOrderService.cs
@@ -0,0 +1,10 @@
+﻿using AspnetRunBasics.Models;
+
+namespace AspnetRunBasics.Services
+{
+    public interface IOrderService
+    {
+        Task<IEnumerable<OrderResponseModel>> GetOrdersByUserName(string userName);
+    }
+
+}
